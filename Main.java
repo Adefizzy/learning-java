6d6d80a5dead877e010e0b882d3ba2ad8207c89c
Adefisayo Adeniji <Adefizzy045@yahoo.com>
2025-10-10 01:40:35 +0100

    learned thread, method overloading, overriding

--- Main.java
+++ Main.java
@@ -16,11 +16,71 @@ class B implements A{
     }
 }
 
+enum Status {
+    Running, Failed, Pending, Success
+}
+
+class PrintString {
+
+    String input;
+    public PrintString(String input){
+        this.input = input;
+
+    }
+
+    public void print(){
+        for (int i = 0;  i < 10; i++){
+            System.out.println(this.input);
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public  void print(int count, int sleep){
+        for (int i = 0;  i < count; i++){
+            System.out.println(this.input);
+            try {
+                Thread.sleep(sleep);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
+
 public class Main {
     public static void main(String[] arg) {
        A b = new B();
 
        b.show();
        b.config();
+
+        Status s = Status.Running;
+
+        switch (s){
+            case Status.Running -> System.out.println("in progress");
+            case Status.Failed -> System.out.println("failed status");
+            case Status.Pending -> System.out.println("pending status");
+            default -> System.out.println("Successful");
+        }
+
+        Runnable first = () -> {
+            PrintString printer = new PrintString("h1");
+            printer.print();
+        };
+
+        Runnable second = () -> {
+            PrintString printer = new PrintString("hello");
+            printer.print(10, 5);
+        };
+
+        Thread firstThread = new Thread(first);
+        Thread secondThread = new Thread(second);
+
+        firstThread.start();
+        secondThread.start();
     }
 }
